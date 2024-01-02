0bea25b1fa122a210d9ed7b38450173bac2ea5a5
soicchi <soicchi@icloud.com>
2024-01-02 15:57:41 +0900

    Created repl package

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 go_test:
 	docker compose run --rm interpreter go test -v -cover ./...
+
+go_run:
+	docker compose run --rm interpreter go run main.go
---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"io"
+	"fmt"
+
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		lexer := lexer.New(line)
+
+		for tok := lexer.NextToken(); tok.Type != token.EOF; tok = lexer.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
